2bf61a5525df00d8fe6784df8ff653333c71c2f4
Michel Aguena <aguena@if.usp.br>
2015-04-21 19:00:53 -0300

    added scale conversions in Gravitation.py

--- Gravitation/Gravitation.py
+++ Gravitation/Gravitation.py
@@ -7,30 +7,39 @@ import numpy as np
 from RK import *
 
 def float_list(LIST): return [float(l) for l in LIST]
-
+best_mass	= 1
+best_distance	= 1
+best_time	= 1
+# distance : 1.5E+11 m (averange distance beetween Earth to sun)
+# mass : 5.9736E+24 kg (Earth mass)
+# time: 3.1104E07: 
+best_mass	= 1.5E+11
+best_distance	= 5.9736E+24 
+best_time	= 3.1104E07
 class Gravitation(object):
     """
     This class is the main Gravitaion wrapper
     """
-    def __init__(self):
+    def __init__(self,scale_mass=best_mass,scale_distance=best_distance,scale_time=best_time):
         """ Compose a list and a dict with all bodies 
 		.bodies  corresponds to a list with the bodies, i. e. [Body1,Body2,...]		=> GRAV.bodies[1]   = Body1 
 		.lookup  corresponds to a dict with the bodies, i. e. dict([id1,Body1],...)	=> GRAV.lookup[id1] = Body1
 	"""
         self.bodies	= []
         self.lookup	= dict([[body.obj_id,body] for body in self.bodies])
+	self.convert_m	= scale_mass	/ best_mass	
+	self.convert_r	= scale_distance/ best_distance
+	self.convert_t	= scale_time	/ best_time	
+	self.convert_v	= self.convert_r/ self.convert_t	
 	self.step_size	= 0.1
     def add_body(self, obj_id, obj_mass, obj_position, obj_velocity):
         """ Add a body to the list with all bodies """
-	#
 	# All dimensions are scaled
-	#
-	# distance : 1.5E+11 m (averange distance beetween Earth to sun)
-	# mass : 5.9736E+24 kg (Earth mass)
-	# time: 3.1104E07: 
-	
+	mass		= float(obj_mass)*self.convert_m
+	position	= [pos*self.convert_r for pos in obj_position]	
+	velocity	= [vel*self.convert_v for vel in obj_velocity]	
+	new_Body	= Body(obj_id,mass,position,velocity)
 
-	new_Body	= Body(obj_id, obj_mass, obj_position, obj_velocity)
 	self.bodies.append( new_Body )
 	self.lookup[obj_id] = new_Body
 	print('*** Body = ',obj_id, obj_mass, obj_position, obj_velocity,' added ***',sep='\t')
