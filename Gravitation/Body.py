40fe18543ded4df832f7b6cf92a28e7575629b85
fnbellomo <fnbellomo@gmail.com>
2015-04-22 23:20:17 -0300

    Structuring the code to make it readable by humans

--- Gravitation/Body.py
+++ Gravitation/Body.py
@@ -1,41 +1,51 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
+
 from __future__ import print_function
-#from toy_funcs import *
-import numpy as np
+
+#import numpy as np
+from numpy import sqrt as np_sqrt
 
 
 class Body(object):
-    """This class represents a Body"""
+    """
+    This class represents a Body
+    """
+
     def __init__(self, obj_id, obj_mass, obj_position, obj_velocity):
-        """The components of the body are"""
-	self.obj_id      	= obj_id		# id		, can be any format
-	self.obj_mass    	= float(obj_mass)	# mass		, float format
-	self.obj_position	= obj_position		# position	, list=[float,float] format
-	self.obj_velocity	= obj_velocity		# velocities	, list=[float,float] format
-	self.obj_path		= [[self.obj_position[0]],[self.obj_position[1]]]	# path 		, list=[pathx,pathy] format
+        """
+        The components of the body are
+        """
+
+        self.obj_id      	= obj_id		# id		, can be any format
+        self.obj_mass    	= float(obj_mass)	# mass		, float format
+        self.obj_position	= obj_position		# position	, list=[float,float] format
+        self.obj_velocity	= obj_velocity		# velocities	, list=[float,float] format
+        self.obj_path		= [[self.obj_position[0]],[self.obj_position[1]]]	# path 		, list=[pathx,pathy] format
+
     def step(self, step_func, step_size, bodies_list, dict_bodies):
-        """Take a step for the position and velocity of the body"""
+        """
+        Take a step for the position and velocity of the body
+        """
+
         self.obj_position,self.obj_velocity = step_func(step_size,self.obj_id,bodies_list,dict_bodies)
-	print(self.obj_id,self.obj_position,self.obj_velocity)
+        print(self.obj_id,self.obj_position,self.obj_velocity)
 	
     def gfactor(self, other_body):
-	    """This function computes the g_ij factor between this object and other_object"""
-	    #
-	    # g_ij = G * m_j * / d**3
-	    #
+        """
+        This function computes the g_ij factor between this object and other_object
+        """
 	    
-	    # Gravity constant
-#	    G = 6.67384E-11  
-	    G = 0.0374038
+        # Gravity constant
+        G = 0.0374038
 
-	    if self.obj_id != other_body.obj_id:
+        if self.obj_id != other_body.obj_id:
 	    
-	        # Distance beetween objects
-	        dx = self.obj_position[0] - other_body.obj_position[0]
-	        dy = self.obj_position[1] - other_body.obj_position[1]
-	        dist = np.sqrt(dx*dx + dy*dy)
+            # Distance beetween objects
+            dx = self.obj_position[0] - other_body.obj_position[0]
+            dy = self.obj_position[1] - other_body.obj_position[1]
+            dist = np-sqrt(dx*dx + dy*dy)
 	       
-	        return G*other_body.obj_mass/(dist*dist*dist)
+            return G*other_body.obj_mass/(dist*dist*dist)
 	        
-	    else:
-	        return 0
+        else:
+            return 0
--- Gravitation/Plot.py
+++ Gravitation/Plot.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3 
 from __future__ import print_function
+
 import numpy as np
+
 import matplotlib
 #To make interactive plots, I need use TK
 matplotlib.use('TkAgg')
@@ -16,9 +18,23 @@ import matplotlib.cm as cmx
 class make_plot(object):
     """
     Class to make the plots in run time
+    
+    Parameters 
+    ---------- 
+    grav : Gravitacional Object
+
+    This class is responsible for making the plots of simulated orbits of the problem of n-bodies. 
+    To create a make_plot object requires a Gravitational object. 
+    If Gravitational().do_plot == True, the plot is done. In contratio case, no.
     """
 
     def __init__(self, grav):
+        """
+        Create the class
+
+        If Gravitational().do_plot == True, make the plots
+        """
+
         self.grav = grav
         self.number_body = len(self.grav.bodies)
         self.bodies_range = range(self.number_body)
@@ -26,61 +42,72 @@ class make_plot(object):
         self.mass_max = max(self.mass)
         self.circle_radio = [ m/self.mass_max for m in self.mass]
         
-	if grav.do_plot == True:
-	        #Turn interactive mode on.
-	        plt.ion()
-	        #Creo que no es necesario el show
-	        plt.show()
+        if grav.do_plot == True:
+            #Turn interactive mode on.
+            plt.ion()
+            plt.show()
 	        
-	        #Setup the plot
-	        self.fig, self.axes = plt.subplots(figsize=(12,12))
-	        #Set x, y label and title
-		label=r'A.U.($6\times10^{24}$m)'
-	        self.axes.set_xlabel(label)
-	        self.axes.set_ylabel(label)
-	        self.axes.set_title('Solution of %s body problems' %(self.number_body))
+            #Setup the plot
+            self.fig, self.axes = plt.subplots(figsize=(12,12))
+            #Set x, y label and title
+            label=r'A.U.($6\times10^{24}$m)'
+            self.axes.set_xlabel(label)
+            self.axes.set_ylabel(label)
+            self.axes.set_title('Solution of %s body problems' %(self.number_body))
 	        
-	        #To make that the same body have the same color
-	        curves = [np.random.random(20) for i in self.bodies_range]
-	        jet = cm = plt.get_cmap('jet') 
-	        cNorm  = colors.Normalize(vmin=0, vmax=self.bodies_range[-1])
-	        self.scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
+            #To make that the same body have the same color
+            curves = [np.random.random(20) for i in self.bodies_range]
+            jet = cm = plt.get_cmap('jet') 
+            cNorm  = colors.Normalize(vmin=0, vmax=self.bodies_range[-1])
+            self.scalarMap = cmx.ScalarMappable(norm=cNorm, cmap=jet)
 	        
-	        #Plot the initial positons and the radios of eacho body in function of this mass
-		circles = []
-	        for i in self.bodies_range:
-		    name = self.grav.bodies[i].obj_id
-	            x = self.grav.bodies[i].obj_position[0]
-	            y = self.grav.bodies[i].obj_position[1]
-	            colorVal = self.scalarMap.to_rgba(i)
-	            #self.axes.plot(x, y, '^', color=colorVal)
-	            circles.append( self.axes.add_patch(plt.Circle((x,y), radius=self.circle_radio[i], color=colorVal,label=name)) )
-	        self.axes.axis('equal')
-	        self.axes.margins(0)
-		plt.legend()
-	        plt.draw()
-	        for obj in circles:
-			obj.remove()
+	    #Plot the initial positons and the radios of eacho body in function of this mass
+            circles = []
+            for i in self.bodies_range:
+                name = self.grav.bodies[i].obj_id
+                x = self.grav.bodies[i].obj_position[0]
+                y = self.grav.bodies[i].obj_position[1]
+                colorVal = self.scalarMap.to_rgba(i)
+                circles.append( self.axes.add_patch(plt.Circle((x,y), radius=self.circle_radio[i], color=colorVal,label=name)) )
+            self.axes.axis('equal')
+            self.axes.margins(0)
+            plt.legend()
+            plt.grid(True)
+            plt.draw()
+            for obj in circles:
+                obj.remove()
     
     def update(self,step_num):
-        #x and y are position vectors for all bodys
-	plot_circ = []; plot_line = []
+        """
+        Update the new points in the plots
+
+        parameters
+        ----------
+        step_num : Number of step
+        """
+
+        #x and y are position vectors for each bodys
+        plot_circ = []
+        plot_line = []
+
         for i in self.bodies_range:
             x  = self.grav.bodies[i].obj_position[0]
             y  = self.grav.bodies[i].obj_position[1]
             xp = self.grav.bodies[i].obj_path[0]
             yp = self.grav.bodies[i].obj_path[1]
+
             colorVal = self.scalarMap.to_rgba(i)
-#            plot_obj.append( self.axes.plot(x, y, 'o', color=colorVal)	)
             plot_circ.append( self.axes.add_patch(plt.Circle((x,y), radius=self.circle_radio[i], color=colorVal)) )
             plot_line.append( self.axes.plot(xp, yp, '--', color=colorVal))
-	time = step_num*self.grav.step_size
-	txt = plt.text(.5,.975,'time='+str(time)+'years',horizontalalignment='center',verticalalignment='center',transform = self.axes.transAxes,bbox=dict(facecolor='1'))
+
+        time = step_num*self.grav.step_size
+        txt = plt.text(.5,.975,'time='+str(time)+'years',horizontalalignment='center',verticalalignment='center',\
+                        transform = self.axes.transAxes,bbox=dict(facecolor='1'))
         plt.draw()
-	for obj in plot_circ:
-		print(obj)
-		obj.remove()
-	for obj in plot_line:
-		print(obj[0])
-		obj[0].remove()
-	txt.remove()
+
+        for obj in plot_circ:
+            obj.remove()
+        for obj in plot_line:
+            obj[0].remove()
+
+        txt.remove()
