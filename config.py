f94a0d5f2b1c083823a84e3f9c59f70c23080ecb
Ezequiel Fogliatto <ezefogliatto@gmail.com>
2015-04-24 01:00:33 -0300

    data change

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-filename	= 'test_bodies.dat'
-method		= 'runge-kutta4'
-tstep		= 1
-number_of_steps	= 20000
-plot_every_n	= 200
-do_plot		= True #False
-save_plot	= True #False
+filename	= 'david.txt'
+method		= 'adaptive-rk4'
+tstep		= 0.00001
+number_of_steps	= 20000000
+plot_every_n	= 10000
+do_plot		= False
+save_plot	= True
